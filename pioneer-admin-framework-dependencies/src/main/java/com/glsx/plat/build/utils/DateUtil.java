c902fd06c722930a72920e4f5fc71c7ac5491fe3
杨林龙 <yangll@didihu.com.cn>
2022-11-15 14:17:50 +0800

    优化基本框架-优化工具类

---
+++ pioneer-admin-framework-dependencies/src/main/java/com/glsx/plat/build/utils/DateUtil.java
@@ -0,0 +1,19 @@
+package com.glsx.plat.build.utils;
+
+
+import java.util.Date;
+
+
+/**
+ * 日期工具类
+ *
+ * @author yangll@didihu.com.cn
+ * @version 1.0
+ * @history 2017年9月7日 create by yangll@didihu.com.cn
+ */
+public class DateUtil {
+
+    public static void main(String[] args) {
+        System.out.println(new Date());
+    }
+}
